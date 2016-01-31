62b325997a26f195d70b28ff2f5def24d1935e0a
dev-ladmusician <dev.ladmusician@gmail.com>
2016-01-31 19:32:56 +0900

    main title 정중앙에 위치시키기!

--- public/js/main/index.js
+++ public/js/main/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
-   // var windowHeight = $(window).height();
-   //$('.gnb-main-section').css('height', windowHeight);
+    var windowHeight = $(window).height();
+    $('.gnb-main-section').css('height', windowHeight);
+    $('.gnb-main-container').css('height', windowHeight);
+
     $(function() {
         var $video = $('#video-main');
         $video.on('canplaythrough', function() {
