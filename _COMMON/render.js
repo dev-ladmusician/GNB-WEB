75c760729e43487248e202b254469afb29aa0ba5
dev-ladmusician <dev.ladmusician@gmail.com>
2016-01-31 18:48:59 +0900

    partialView 사용하는 engine을 구현하려다 실패하였습니다. ㅈ

--- _COMMON/render.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    renderView: function (res, view, data) {
-        //res.render('_COMMON/header');
-        res.render(view, data);
-        //res.render('_COMMON/footer');
-    }
-}
--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var render = require('../_COMMON/render');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    render.renderView(res, '_MAIN/index', { title: 'Express' });
+    res.render('_MAIN/index', { title: 'Express' });
 });
 
 module.exports = router;
