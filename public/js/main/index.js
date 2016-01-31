338cecf4b26a26aace651ab118272bf48d8133fa
dev-ladmusician <dev.ladmusician@gmail.com>
2016-01-31 19:25:31 +0900

    main video 넣기 (snoopy꺼 말도없이 훔쳐오는데, 나중에 걸릴지도모름)

---
+++ public/js/main/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+   // var windowHeight = $(window).height();
+   //$('.gnb-main-section').css('height', windowHeight);
+    $(function() {
+        var $video = $('#video-main');
+        $video.on('canplaythrough', function() {
+            this.play();
+        });
+    });
+});
