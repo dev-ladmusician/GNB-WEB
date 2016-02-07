e71c20a9580d0d3359469b411d7a0452f2990661
dev-ladmusician <dev.ladmusician@gmail.com>
2016-02-07 17:16:36 +0900

    로그인 error-msg 나타내도록 구

--- public/js/main/index.js
+++ public/js/main/index.js
@@ -8,7 +8,15 @@ $(document).ready(function () {
 
 var app = angular.module('gnbApp', []).
 controller('MainCtrl', function ($scope) {
-    $scope.test= true;
     $scope.submit = function () {
+        if ($scope.username && $scope.username.length == 10) {
+            if ($scope.password && $scope.password.length > 4) {
+
+            } else {
+                $('.error-msg').html("정확한 비밀번호를 입력하세요.");
+            }
+        } else {
+            $('.error-msg').html("올바른 학번을 입력하세요.");
+        }
     }
 });
